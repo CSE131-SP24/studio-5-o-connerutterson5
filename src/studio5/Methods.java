c3c09b967b8ef46f53691f899c7827874a612bfc
henry <henry@HenryUYoga.dhcp.wustl.edu>
2024-02-28 17:16:41 -0600

    Studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -13,10 +13,11 @@ public class Methods {
 	 * @param y2 y-coordinate of another point
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
+
+	
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		double distance = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 		return distance;
 	}
 
@@ -30,7 +31,12 @@ public class Methods {
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
-
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, 3 * radius / 4);
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius / 2);
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius / 4);
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
@@ -61,6 +67,19 @@ public class Methods {
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
+		for(int i = 0; i < source.length(); i++){
+			if (source.charAt(i) == target) {
+				result += replacement;
+			}
+			else {
+			result += source.charAt(i);
+				
+			}
+		}
+				
+		
+		
+	
 		// TODO: Finish this method
 		
 		return result;
